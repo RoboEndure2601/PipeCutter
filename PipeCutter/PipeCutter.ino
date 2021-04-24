59e5551c05b303acac2abd19feeb5a8e88cc5b0e
roboendure <roboendure@roboendure.com>
2021-04-24 15:54:09 +0530

    ino update

---
+++ PipeCutter/PipeCutter.ino
@@ -0,0 +1,15 @@
+/*==============================================================================
+  PipeCutter Firmware
+  (c) 2011-2020 PipeCutterFirmware
+  Portions of PipeCutter are (c) by their respective authors.
+  All code complies with GPLv2 and/or GPLv3
+================================================================================
+Greetings! Thank you for choosing PipeCutter.
+we recommend the following essential links:
+PIpeCutter Firmware Official Website
+  - https://roboendure.com/
+    The official PipeCutter Firmware website contains the most up-to-date
+    documentation. Contributions are always welcome!
+Configuration
+  - https://github.com/parth9109/PipeCutter/blob/main/PipeCutter/Configuration.h
+------------------------------------------------------------------------------*/
