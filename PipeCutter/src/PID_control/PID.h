001dc527904cff6493187eb0481a6cd4522fdc7b
roboendure <parththesiya@gmail.com>
2021-05-27 18:04:35 +0530

    PID

---
+++ PipeCutter/src/PID_control/PID.h
@@ -0,0 +1,8 @@
+#pragma
+
+#include "src/MotorControl/PanasonicServoA4Mines.h"
+#include "src/encoder/encoder.h"
+
+// doing close loop between Motor and Encoder
+// count encoder pulse and compare to position and make pid connection 
+
